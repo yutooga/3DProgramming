abd48c1c790eabef1b738485893ddecb876e81ef
yutooga <kd1350391@st.kobedenshi.ac.jp>
2024-04-17 11:43:11 +0900

    BaseFrameworkを更新
    a変数を追加＊絶対消す

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,6 +221,8 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
+	a = 5;//消す
+
 	return true;
 }
 
